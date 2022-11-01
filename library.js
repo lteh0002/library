9458af232753c763e72bde97c7f76a19ffc0795f
lteh0002 <tehlinhui@gmail.com>
2022-11-01 21:01:20 +0800

    Add DOM for user input and button

--- library.js
+++ library.js
@@ -1,3 +1,7 @@
+let titleInput = document.querySelector('#title');
+let authorInput = document.querySelector('#author');
+let pageInput = document.querySelector('#page');
+let submit = document.querySelector('.submit');
 let myLibrary = [];
 
 function Book() {
