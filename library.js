79de8b40da348f09f61c26006ed23c7f4b5936b2
lteh0002 <tehlinhui@gmail.com>
2022-11-01 21:02:11 +0800

    Added constructor function for Book

--- library.js
+++ library.js
@@ -4,9 +4,12 @@ let pageInput = document.querySelector('#page');
 let submit = document.querySelector('.submit');
 let myLibrary = [];
 
-function Book() {
-  // the constructor...
-}
+function Book(title, author, page) {
+    // the constructor...
+    this.title = title;
+    this.author = author;
+    this.page = page;
+  }
 
 function addBookToLibrary() {
   // do stuff here
